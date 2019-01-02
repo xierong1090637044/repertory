4b29da60ae750a30b52b4932a50f9edf817f8ffd
xr <xr@wolvestribe.com>
2019-01-02 15:10:23 +0800

    操作记录增加查看tab选项

--- pages/order_history/order_history.js
+++ pages/order_history/order_history.js
@@ -4,12 +4,25 @@ Page({
 
   /*** 页面的初始数据*/
   data: {
+    current: '1',
+  },
+
+  handleChange({ detail }) {
+    this.setData({
+      current: detail.key,
+    });
+    if (detail.key == 1)
+    {
+      that.get_list("day");
+    }else{
+      that.get_list("month");
+    }
   },
 
   /*** 生命周期函数--监听页面加载*/
   onLoad: function (options) {
     that = this;
-    that.get_list();
+    that.get_list("day");
   },
 
   /*** 生命周期函数--监听页面初次渲染完成*/
@@ -47,12 +60,20 @@ Page({
 
   },
 
-  get_list:function()
+  get_list:function(type)
   {
+    console.log(type)
     that.setData({ spinShow:true});
     var userid = wx.getStorageSync("userid");
     const query = Bmob.Query("order_opreations");
     query.equalTo("master", "==", userid);
+    if(type =="day")
+    {
+      query.equalTo("createdAt", ">", that.getDay(-7));
+    }else{
+      query.equalTo("createdAt", ">", that.getDay(-30));
+    }
+    
     query.order("-createdAt");
     query.find().then(res => {
       console.log(res);
@@ -70,5 +91,26 @@ Page({
     wx.navigateTo({
       url: 'detail/detail?id='+id,
     })
-  }
+  },
+
+  getDay: function (day) {
+    var that = this;
+    var today = new Date();
+    var targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * day;
+    today.setTime(targetday_milliseconds);
+    var tYear = today.getFullYear();
+    var tMonth = today.getMonth();
+    var tDate = today.getDate();
+    tMonth = that.handleMonth(tMonth + 1);
+    tDate = that.handleMonth(tDate);
+    return tYear + "-" + tMonth + "-" + tDate + " " + "00:00:00";
+  },
+
+  handleMonth: function (month) {
+    var m = month;
+    if (month.toString().length == 1) {
+      m = "0" + month;
+    }
+    return m;
+  },
 })
--- pages/order_history_fri/order_history_fri.js
+++ pages/order_history_fri/order_history_fri.js
@@ -4,13 +4,24 @@ Page({
 
   /*** 页面的初始数据*/
   data: {
-    
+    current: '1',
+  },
+
+  handleChange({ detail }) {
+    this.setData({
+      current: detail.key,
+    });
+    if (detail.key == 1) {
+      that.get_list_check("day");
+    } else {
+      that.get_list_check("month");
+    }
   },
 
   /*** 生命周期函数--监听页面加载*/
   onLoad: function (options) {
     that = this;
-    that.get_list_check();
+    that.get_list_check("day");
   },
 
   /*** 生命周期函数--监听页面初次渲染完成*/
@@ -48,11 +59,16 @@ Page({
 
   },
 
-  get_list_check: function () {
+  get_list_check: function (type) {
     that.setData({ spinShow: true });
     var userid = wx.getStorageSync("userid");
     const query = Bmob.Query("order_opreations_fri");
     query.equalTo("master", "==", userid);
+    if (type == "day") {
+      query.equalTo("createdAt", ">", that.getDay(-7));
+    } else {
+      query.equalTo("createdAt", ">", that.getDay(-30));
+    }
     query.order("-createdAt");
     query.find().then(res => {
       console.log(res);
@@ -69,5 +85,26 @@ Page({
       wx.navigateTo({
         url: 'detail/detail?id=' + id,
       })
-  }
+  },
+
+  getDay: function (day) {
+    var that = this;
+    var today = new Date();
+    var targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * day;
+    today.setTime(targetday_milliseconds);
+    var tYear = today.getFullYear();
+    var tMonth = today.getMonth();
+    var tDate = today.getDate();
+    tMonth = that.handleMonth(tMonth + 1);
+    tDate = that.handleMonth(tDate);
+    return tYear + "-" + tMonth + "-" + tDate + " " + "00:00:00";
+  },
+
+  handleMonth: function (month) {
+    var m = month;
+    if (month.toString().length == 1) {
+      m = "0" + month;
+    }
+    return m;
+  },
 })
