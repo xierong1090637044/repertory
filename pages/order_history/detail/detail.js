5cb2920211fa33c4dea572b3113c5284bb62c3da
xr <xr@wolvestribe.com>
2019-01-07 15:51:29 +0800

    增加可以选择客户选项

--- pages/order_history/detail/detail.js
+++ pages/order_history/detail/detail.js
@@ -29,7 +29,7 @@ Page({
   {
     that.setData({ spinShow:true});
     const query = Bmob.Query('order_opreations');
-    query.include("opreater");
+    query.include("opreater","custom");
     query.get(id).then(res => {
       console.log(res);
       that.setData({detail:res});
