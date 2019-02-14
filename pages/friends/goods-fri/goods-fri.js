447d25149a6011e37cf89461f696156fa9ca8c4b
xr <xr@wolvestribe.com>
2019-02-14 14:35:53 +0800

    协同管理好友产品以及入库操作更新

--- pages/friends/goods-fri/goods-fri.js
+++ pages/friends/goods-fri/goods-fri.js
@@ -1,8 +1,15 @@
 // pages/goods/goods.js
-const Bmob = require('../../../utils/bmob.js')
+const Bmob = require('../../../utils/bmob.js');
+const Bmob_new = require('../../../utils/bmob_new.js')
 var config = require('../../../utils/config.js')
 var _ = require('../../../utils/we-lodash.js');
 var userid = '';
+var now_product;
+var class_text;
+var that;
+var type;//库存情况
+var class_array;//产品类别
+var select_id = null;//类别选择的id
 Page({
 
   /**
@@ -11,17 +18,39 @@ Page({
   data: {
     spinShow: true,
     goods: [],
-    currentPage: 0, //要跳过查询的页数
-    limitPage: config.pageSize,//首先显示3条数据（之后加载时都增加3条数据，直到再次加载不够3条）
-    isEmpty: true, //当前查询出来的数据是否为空
+    limitPage: 200,//限制显示条数
+    isEmpty: false, //当前查询出来的数据是否为空
     isEnd: false, //是否到底了
-    endPage: 0, //最后一页加载多少条
-    totalPage: 0, //总页数
     totalGoods: [],
     // 搜索
     inputShowed: false,
-    inputVal: ""
+    inputVal: "",
+    current: '1',
+    length: null,
   },
+
+  //选择库存情况
+  bindstock_Change: function (e) {
+    if (e.detail.value == "0") {
+      that.loadGoods(true, null, select_id);
+      that.setData({ selectd_stock: that.data.stock[e.detail.value] });
+      type = true;
+    } else {
+      that.loadGoods(false, null, select_id);
+      that.setData({ selectd_stock: that.data.stock[e.detail.value] });
+      type = false;
+    }
+  },
+
+  //选择产品类别
+  bindclass_Change: function (e) {
+    var index = e.detail.value;
+    select_id = class_array[index].objectId;
+    that.setData({ selectd_class: class_array[index].class_text });
+    that.loadGoods(type, null, select_id);
+
+  },
+
   // 搜索
   showInput: function () {
     this.setData({
@@ -39,7 +68,6 @@ Page({
       inputVal: ""
     });
     this.handleResetData()
-    this.loadAll()
     this.loadGoods()
   },
   inputTyping: function (e) {
@@ -47,52 +75,37 @@ Page({
       inputVal: e.detail.value
     });
   },
+
   searchAction: function (e) {
     var that = this;
     var inputVal = this.data.inputVal
-    var filterGoods = _.chain(that.data.totalGoods)
-      .filter(function (res) {
-        return res.goodsName.match(new RegExp(inputVal));
-      })
-      .map(function (res) {
-        return res;
-      })
-      .value();
-    var isEmpty = true
-    if (filterGoods.length) {
-      isEmpty = false
-    }
-    that.setData({
-      goods: filterGoods,
-      isEmpty: isEmpty,
-      isEnd: !isEmpty
-    })
+    that.loadGoods(type, inputVal);
   },
-  // /.搜索
+
+  //产品点击
   handleDetial: function (e) {
-    var item = e.target.dataset.item
-    wx.setStorageSync('item', JSON.stringify(item))
-    wx.navigateTo({
-      url: '/pages/common/goods-dtl/goods-dtl?type=1'
-    })
   },
+
   handleEditGoods: function (e) {
     var that = this
-    var item = e.currentTarget.dataset.item
+    var item = now_product;
     wx.setStorageSync('editGoods', item)
     wx.navigateTo({
       url: '/pages/goods/goods-edit/goods-edit',
     })
   },
-  handleDelGoods: function (e) {
+
+  handleDelGoods: function () {
     var that = this
-    var item = e.currentTarget.dataset.item
-    var id = e.currentTarget.dataset.item.goodsId
+    var item = now_product;
+    console.log(now_product);
+    var id = now_product.goodsId
     wx.showModal({
       title: '提示',
       content: '是否删除【' + item.goodsName + '】产品',
       success: function (res) {
         if (res.confirm) {
+          wx.showLoading({ title: '删除中...', });
           var BillsTemp = Bmob.Object.extend("BillsTemp");
           var queryBillsTemp = new Bmob.Query(BillsTemp);
           queryBillsTemp.equalTo("goodsId", id);
@@ -129,11 +142,12 @@ Page({
                   success: function (result) {
                     result.destroy({
                       success: function (res) {
+                        wx.hideLoading();
                         wx.showToast({
                           title: '删除成功',
                           icon: 'success'
                         })
-                        that.onShow();
+                        that.onLoad();
                       },
                       error: function (result, error) {
                         console.log(error);
@@ -163,17 +177,34 @@ Page({
     })
   },
 
-  loadGoods: function () {
+  loadGoods: function (type, content, class_id) {
     var that = this;
+    that.setData({ spinShow: true });
     var Goods = Bmob.Object.extend("Goods");
     var query = new Bmob.Query(Goods);
     query.equalTo("userId", userid);
+    if (type == true) {
+      query.greaterThan("reserve", 0);//库存充足
+    } else if (type == false) {
+      query.lessThanOrEqualTo("reserve", 0);//库存紧张
+    } else { }
+
+    if (content != null) query.equalTo("goodsName", { "$regex": "" + content + ".*" });
+    if (class_id != null) query.equalTo("goodsClass", class_id);
+
     query.limit(that.data.limitPage);
-    query.skip(that.data.limitPage * that.data.currentPage);
-    query.descending("createdAt"); //按照时间降序
+    query.descending("goodsName"); //按照时间降序
     query.include("userId");
+    query.include("goodsClass");
     query.find({
       success: function (res) {
+        that.setData({ length: res.length });
+        if (res.length == 0) {
+          that.setData({ contentEmpty: true })
+        } else {
+          that.setData({ contentEmpty: false })
+        }
+
         var tempGoodsArr = new Array();
         for (var i = 0; i < res.length; i++) {
           var tempGoods = {}
@@ -188,115 +219,37 @@ Page({
           tempGoods.productCode = res[i].get("productCode") || '';
           tempGoods.packageContent = res[i].get("packageContent") || '';
           tempGoods.packingUnit = res[i].get("packingUnit") || '';
+          tempGoods.packModel = res[i].get("packModel") || '';
           tempGoods.reserve = res[i].get("reserve") || 0;
           tempGoods.costPrice = res[i].get("costPrice") || 0;
           tempGoods.retailPrice = res[i].get("retailPrice") || 0;
-          tempGoods.qrCode = config.api.fetchQRCode + res[i].id || '';
+          tempGoods.class_text = res[i].get("goodsClass") || '';
           tempGoodsArr.push(tempGoods);
         }
         that.handleData(tempGoodsArr);
       }
     })
   },
-  //加载下一页
-  loadMore: function () {
-    var that = this;
-    wx.showLoading({
-      title: '正在加载',
-      mask: true
-    });
-    //一秒后关闭加载提示框
-    setTimeout(function () {
-      wx.hideLoading()
-    }, 1000)
 
-    that.setData({
-      currentPage: that.data.currentPage + 1
-    });
-    //先判断是不是最后一页
-    if (that.data.currentPage + 1 == that.data.totalPage) {
-      that.setData({
-        isEnd: true
-      })
-      // if (that.data.endPage != 0) { //如果最后一页的加载不等于0
-      //   that.setData({
-      //     limitPage: that.data.endPage,
-      //   })
-      // }
-      this.loadGoods();
-    } else {
-      this.loadGoods();
-    }
-  },
-  loadAll: function () {
+  //得到总库存数和总金额
+  loadallGoods: function (class_id) {
     var that = this;
+    var total_reserve = 0;
+    var total_money = 0;
     var Goods = Bmob.Object.extend("Goods");
     var query = new Bmob.Query(Goods);
     query.equalTo("userId", userid);
-    query.descending("createdAt"); //按照时间降序
-    query.include("userId");
-    query.limit(1000)
+    if (class_id != null) query.equalTo("goodsClass", class_id);
     query.find({
       success: function (res) {
-        var count = res.length;
-        var totalPage = 0;
-        var endPage = 0;
-        if (count == 0) {
-          that.setData({
-            isEmpty: true
-          })
-        }
-        else if (count % that.data.limitPage == 0) {
-          totalPage = parseInt(count / that.data.limitPage);
-          if (totalPage == 1) {
-            that.setData({
-              isEnd: true,
-              isEmpty: false
-            })
-          }
-        } else {
-          var lowPage = parseInt(count / that.data.limitPage);
-          totalPage = lowPage + 1;
-          if (lowPage == 0) {
-            that.setData({
-              isEnd: true,
-              isEmpty: false
-            })
-          } else {
-            endPage = count - (lowPage * that.data.limitPage);
-          }
-        }
+        that.getclass_list();
 
-        var tempGoodsArr = new Array();
         for (var i = 0; i < res.length; i++) {
-          that.setData({
-            isEmpty: false
-          })
-          var tempGoods = {}
-          tempGoods.userid = userid || '';
-          tempGoods.userName = res[i].get("userId").username || '';
-          tempGoods.avatarUrl = res[i].get("userId").avatarUrl || '';
-          tempGoods.goodsId = res[i].id || '';
-          tempGoods.goodsName = res[i].get("goodsName") || '';
-          tempGoods.regNumber = res[i].get("regNumber") || '';
-          tempGoods.producer = res[i].get("producer") || '';
-          tempGoods.productCode = res[i].get("productCode") || '';
-          tempGoods.packageContent = res[i].get("packageContent") || '';
-          tempGoods.packingUnit = res[i].get("packingUnit") || '';
-          tempGoods.reserve = res[i].get("reserve") || 0;
-          tempGoods.costPrice = res[i].get("costPrice") || 0;
-          tempGoods.retailPrice = res[i].get("retailPrice") || 0;
-          tempGoods.qrCode = config.api.fetchQRCode + res[i].id || '';
-          tempGoodsArr.push(tempGoods);
+          total_reserve = total_reserve + res[i].get("reserve");
+          total_money = total_money + res[i].get("reserve") * res[i].get("retailPrice");
         }
-        that.setData({
-          totalCount: count,
-          endPage: endPage,
-          totalPage: totalPage,
-          totalGoods: tempGoodsArr || [],
-          spinShow: false
-        })
-        console.log("【我的产品】【共有" + count + "条记录】 " + "【共有" + totalPage + "页】" + " 【最后一页加载" + endPage + "条】");
+        that.setData({ total_reserve: total_reserve, total_money: total_money });
+        console.log(total_reserve, total_money);
       }
     })
   },
@@ -308,34 +261,70 @@ Page({
     data = data || [];
     this.setData({
       goods: page === 1 || page === undefined ? data : this.data.goods.concat(data),
+      spinShow: false
     });
+  },
+
+  //滚动加载更多
+  loadMore: function () {
+    var that = this;
+    if (that.data.length < that.data.limitPage) {
+      wx.showToast({
+        icon: 'none',
+        title: '到底啦',
+      })
+    } else {
+      that.setData({ limitPage: that.data.limitPage + that.data.limitPage, })
+      that.loadGoods(type, null, select_id);
+    }
+
   },
 
   //重置
   handleResetData: function () {
     this.setData({
       currentPage: 0,
-      limitPage: config.pageSize,
+      limitPage: 200,
       goods: [],
       isEnd: false,
-      isEmpty: true,
+      isEmpty: false,
       inputVal: '',
       inputShowed: false,
-      spinShow: true
+      spinShow: true,
+      current: '1',
+      selectd_stock: "库存情况",
+      stock: ["库存充足", "库存不足"],
+      selectd_class: "产品类别"
     })
   },
 
   handleRefresh: function () {
     this.handleResetData()
-    this.loadAll()
-    this.loadGoods()
+    this.loadGoods(null, null, null)
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  //得到类别列表
+  getclass_list: function () {
+    const query = Bmob_new.Query("class_user");
+    query.equalTo("parent", "==", userid);
+    query.find().then(res => {
+      wx.setStorageSync("class", res);
+
+      var all = {};
+      all.class_text = "全部";
+      all.objectId = null;
+      res.push(all);
+      that.setData({ all_class: res });
+      class_array = res;
+    });
+  },
+
+  /*** 生命周期函数--监听页面加载*/
   onLoad: function (options) {
     userid = wx.getStorageSync("friendId");
+    this.handleRefresh();
+    that = this;
+    //that.loadallGoods();
   },
 
   /**
@@ -349,7 +338,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.handleRefresh()
+    var is_add = wx.getStorageSync("is_add");
+    if (is_add) {
+      this.handleRefresh();
+      wx.setStorageSync("is_add", false);
+    }
+
+    that.getclass_list();
   },
 
   /**
@@ -363,7 +358,8 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    type = null;//库存情况
+    select_id = null;//类别选择的id
   },
 
   /**
@@ -385,5 +381,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
 })
