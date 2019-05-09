4f022684fa24dfbe282a3393cfe70c19072c97e4
XR1090637044 <1090637044@qq.com>
2019-05-09 14:39:04 +0800

    增加今日入库显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -23,12 +23,55 @@ Page({
     ],
   },
 
+  //得到今日概况
+  gettoday_detail: function () {
+    let get_reserve = 0;
+    let out_reserve = 0;
+    let get_reserve_real_money = 0;
+    let out_reserve_real_money = 0;
+    let get_reserve_num = 0;
+    let out_reserve_num = 0;
+
+    const query = Bmob.Query("Bills");
+    query.equalTo("userId", "==", wx.getStorageSync("userid"));
+    query.equalTo("createdAt", ">=", that.getDay(0));
+    query.equalTo("createdAt", "<=", that.getDay(1));
+
+    query.include("goodsId");
+    query.find().then(res => {
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].type == 1) {
+          get_reserve = get_reserve + res[i].num;
+          get_reserve_real_money = get_reserve_real_money + res[i].num * res[i].goodsId.retailPrice;
+          get_reserve_num = get_reserve_num + res[i].total_money;
+        } else if (res[i].type == -1) {
+          out_reserve = out_reserve + res[i].num;
+          out_reserve_real_money = out_reserve_real_money + res[i].num * res[i].goodsId.costPrice;
+          out_reserve_num = out_reserve_num + res[i].total_money;
+        }
+      }
+
+      that.setData({
+        spinShow: true,
+        get_reserve: get_reserve,
+        out_reserve: out_reserve,
+        get_reserve_real_money: get_reserve_real_money,
+        out_reserve_real_money: out_reserve_real_money,
+        get_reserve_num: get_reserve_num.toFixed(2),
+        out_reserve_num: out_reserve_num,
+        get_reserve_get_num: (get_reserve_real_money - get_reserve_num).toFixed(2),
+        out_reserve_get_num: out_reserve_num - out_reserve_real_money,
+      });
+
+    });
+  },
+
   //得到总库存数和总金额
   loadallGoods: function () {
     var total_reserve = 0;
     var total_money = 0;
     const query = Bmob.Query("Goods");
-    query.equalTo("userId", "==", wx.getStorageSync("userid"));
+    query.equalTo("userId", "==", userid);
     query.limit(500);
     query.find().then(res => {
       for (var i = 0; i < res.length; i++) {
@@ -116,6 +159,7 @@ Page({
 
   /*** 生命周期函数--监听页面显示*/
   onShow: function () {
+    that.gettoday_detail();
     that.loadallGoods();
   },
 
@@ -143,6 +187,28 @@ Page({
   onShareAppMessage: function () {
 
   },
+
+  getDay: function (day) {
+    var that = this;
+    var today = new Date();
+    var targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * day;
+    today.setTime(targetday_milliseconds);
+    var tYear = today.getFullYear();
+    var tMonth = today.getMonth();
+    var tDate = today.getDate();
+    tMonth = that.handleMonth(tMonth + 1);
+    tDate = that.handleMonth(tDate);
+    return tYear + "-" + tMonth + "-" + tDate + " " + "00:00:00";
+  },
+
+  handleMonth: function (month) {
+    var m = month;
+    if (month.toString().length == 1) {
+      m = "0" + month;
+    }
+    return m;
+  },
+
 
   skip: function () {
     wx.navigateTo({
