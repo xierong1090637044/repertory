413aa88a2f83fa309854f19fb654eedad9e44f03
XR1090637044 <1090637044@qq.com>
2019-05-20 11:17:32 +0800

    版本更新

--- utils/bmob.js
+++ utils/bmob.js
@@ -9,7 +9,7 @@
 (function (root) {
     var _ = require('underscore.js');
     var Bmob = {};
-    Bmob.VERSION = "js0.0.1";
+    Bmob.VERSION = "js3.7.0";
     Bmob._ = _;
 
     var EmptyConstructor = function () { };
@@ -104,7 +104,7 @@
         Bmob.applicationKey = applicationKey;
         Bmob.masterKey = masterKey;
         Bmob._useMasterKey = true;
-        Bmob.serverURL = "https://" + applicationId + ".bmobcloud.com";
+        Bmob.serverURL = "https://api.bmobcloud.com";
     };
 
     if (Bmob._isNode) {
@@ -223,10 +223,10 @@
         var dataObject = JSON.parse(data);
 
         var error;
-        wx.showNavigationBarLoading()
+
         if (dataObject.category == "wechatApp") {
 
-            wx.uploadFile({
+            const uploadTask = wx.uploadFile({
                 url: url,
                 filePath: dataObject.base64,
                 name: 'file',
@@ -235,17 +235,22 @@
                 },
                 formData: dataObject,
                 success: function (res) {
-                    console.log(res);
                     var data = JSON.parse(res.data);
                     promise.resolve(data, res.statusCode, res);
-                    wx.hideNavigationBarLoading()
+
                 },
                 fail: function (e) {
                     console.log(e);
                     promise.reject(e);
-                    wx.hideNavigationBarLoading()
+
                 }
             });
+
+            uploadTask.onProgressUpdate((res) => {
+                console.log('上传进度', res.progress)
+                console.log('已经上传的数据长度', res.totalBytesSent)
+                console.log('预期需要上传的数据总长度', res.totalBytesExpectedToSend)
+            })
         } else {
 
             wx.request({
@@ -264,11 +269,11 @@
                     } else {
                         promise.resolve(res.data, res.statusCode, res);
                     }
-                    wx.hideNavigationBarLoading()
+
                 },
                 fail: function (e) {
                     promise.reject(e);
-                    wx.hideNavigationBarLoading()
+
                 }
             });
         }
@@ -2549,200 +2554,6 @@
 
     };
 
-    // A list of file extensions to mime types as found here:
-    // http://stackoverflow.com/questions/58510/using-net-how-can-you-find-the-
-    //     mime-type-of-a-file-based-on-the-file-signature
-    var mimeTypes = {
-        ai: "application/postscript",
-        aif: "audio/x-aiff",
-        aifc: "audio/x-aiff",
-        aiff: "audio/x-aiff",
-        asc: "text/plain",
-        atom: "application/atom+xml",
-        au: "audio/basic",
-        avi: "video/x-msvideo",
-        bcpio: "application/x-bcpio",
-        bin: "application/octet-stream",
-        bmp: "image/bmp",
-        cdf: "application/x-netcdf",
-        cgm: "image/cgm",
-        "class": "application/octet-stream",
-        cpio: "application/x-cpio",
-        cpt: "application/mac-compactpro",
-        csh: "application/x-csh",
-        css: "text/css",
-        dcr: "application/x-director",
-        dif: "video/x-dv",
-        dir: "application/x-director",
-        djv: "image/vnd.djvu",
-        djvu: "image/vnd.djvu",
-        dll: "application/octet-stream",
-        dmg: "application/octet-stream",
-        dms: "application/octet-stream",
-        doc: "application/msword",
-        docx: "application/vnd.openxmlformats-officedocument.wordprocessingml." + "document",
-        dotx: "application/vnd.openxmlformats-officedocument.wordprocessingml." + "template",
-        docm: "application/vnd.ms-word.document.macroEnabled.12",
-        dotm: "application/vnd.ms-word.template.macroEnabled.12",
-        dtd: "application/xml-dtd",
-        dv: "video/x-dv",
-        dvi: "application/x-dvi",
-        dxr: "application/x-director",
-        eps: "application/postscript",
-        etx: "text/x-setext",
-        exe: "application/octet-stream",
-        ez: "application/andrew-inset",
-        gif: "image/gif",
-        gram: "application/srgs",
-        grxml: "application/srgs+xml",
-        gtar: "application/x-gtar",
-        hdf: "application/x-hdf",
-        hqx: "application/mac-binhex40",
-        htm: "text/html",
-        html: "text/html",
-        ice: "x-conference/x-cooltalk",
-        ico: "image/x-icon",
-        ics: "text/calendar",
-        ief: "image/ief",
-        ifb: "text/calendar",
-        iges: "model/iges",
-        igs: "model/iges",
-        jnlp: "application/x-java-jnlp-file",
-        jp2: "image/jp2",
-        jpe: "image/jpeg",
-        jpeg: "image/jpeg",
-        jpg: "image/jpeg",
-        js: "application/x-javascript",
-        kar: "audio/midi",
-        latex: "application/x-latex",
-        lha: "application/octet-stream",
-        lzh: "application/octet-stream",
-        m3u: "audio/x-mpegurl",
-        m4a: "audio/mp4a-latm",
-        m4b: "audio/mp4a-latm",
-        m4p: "audio/mp4a-latm",
-        m4u: "video/vnd.mpegurl",
-        m4v: "video/x-m4v",
-        mac: "image/x-macpaint",
-        man: "application/x-troff-man",
-        mathml: "application/mathml+xml",
-        me: "application/x-troff-me",
-        mesh: "model/mesh",
-        mid: "audio/midi",
-        midi: "audio/midi",
-        mif: "application/vnd.mif",
-        mov: "video/quicktime",
-        movie: "video/x-sgi-movie",
-        mp2: "audio/mpeg",
-        mp3: "audio/mpeg",
-        mp4: "video/mp4",
-        mpe: "video/mpeg",
-        mpeg: "video/mpeg",
-        mpg: "video/mpeg",
-        mpga: "audio/mpeg",
-        ms: "application/x-troff-ms",
-        msh: "model/mesh",
-        mxu: "video/vnd.mpegurl",
-        nc: "application/x-netcdf",
-        oda: "application/oda",
-        ogg: "application/ogg",
-        pbm: "image/x-portable-bitmap",
-        pct: "image/pict",
-        pdb: "chemical/x-pdb",
-        pdf: "application/pdf",
-        pgm: "image/x-portable-graymap",
-        pgn: "application/x-chess-pgn",
-        pic: "image/pict",
-        pict: "image/pict",
-        png: "image/png",
-        pnm: "image/x-portable-anymap",
-        pnt: "image/x-macpaint",
-        pntg: "image/x-macpaint",
-        ppm: "image/x-portable-pixmap",
-        ppt: "application/vnd.ms-powerpoint",
-        pptx: "application/vnd.openxmlformats-officedocument.presentationml." + "presentation",
-        potx: "application/vnd.openxmlformats-officedocument.presentationml." + "template",
-        ppsx: "application/vnd.openxmlformats-officedocument.presentationml." + "slideshow",
-        ppam: "application/vnd.ms-powerpoint.addin.macroEnabled.12",
-        pptm: "application/vnd.ms-powerpoint.presentation.macroEnabled.12",
-        potm: "application/vnd.ms-powerpoint.template.macroEnabled.12",
-        ppsm: "application/vnd.ms-powerpoint.slideshow.macroEnabled.12",
-        ps: "application/postscript",
-        qt: "video/quicktime",
-        qti: "image/x-quicktime",
-        qtif: "image/x-quicktime",
-        ra: "audio/x-pn-realaudio",
-        ram: "audio/x-pn-realaudio",
-        ras: "image/x-cmu-raster",
-        rdf: "application/rdf+xml",
-        rgb: "image/x-rgb",
-        rm: "application/vnd.rn-realmedia",
-        roff: "application/x-troff",
-        rtf: "text/rtf",
-        rtx: "text/richtext",
-        sgm: "text/sgml",
-        sgml: "text/sgml",
-        sh: "application/x-sh",
-        shar: "application/x-shar",
-        silo: "model/mesh",
-        sit: "application/x-stuffit",
-        skd: "application/x-koan",
-        skm: "application/x-koan",
-        skp: "application/x-koan",
-        skt: "application/x-koan",
-        smi: "application/smil",
-        smil: "application/smil",
-        snd: "audio/basic",
-        so: "application/octet-stream",
-        spl: "application/x-futuresplash",
-        src: "application/x-wais-source",
-        sv4cpio: "application/x-sv4cpio",
-        sv4crc: "application/x-sv4crc",
-        svg: "image/svg+xml",
-        swf: "application/x-shockwave-flash",
-        t: "application/x-troff",
-        tar: "application/x-tar",
-        tcl: "application/x-tcl",
-        tex: "application/x-tex",
-        texi: "application/x-texinfo",
-        texinfo: "application/x-texinfo",
-        tif: "image/tiff",
-        tiff: "image/tiff",
-        tr: "application/x-troff",
-        tsv: "text/tab-separated-values",
-        txt: "text/plain",
-        ustar: "application/x-ustar",
-        vcd: "application/x-cdlink",
-        vrml: "model/vrml",
-        vxml: "application/voicexml+xml",
-        wav: "audio/x-wav",
-        wbmp: "image/vnd.wap.wbmp",
-        wbmxl: "application/vnd.wap.wbxml",
-        wml: "text/vnd.wap.wml",
-        wmlc: "application/vnd.wap.wmlc",
-        wmls: "text/vnd.wap.wmlscript",
-        wmlsc: "application/vnd.wap.wmlscriptc",
-        wrl: "model/vrml",
-        xbm: "image/x-xbitmap",
-        xht: "application/xhtml+xml",
-        xhtml: "application/xhtml+xml",
-        xls: "application/vnd.ms-excel",
-        xml: "application/xml",
-        xpm: "image/x-xpixmap",
-        xsl: "application/xml",
-        xlsx: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        xltx: "application/vnd.openxmlformats-officedocument.spreadsheetml." + "template",
-        xlsm: "application/vnd.ms-excel.sheet.macroEnabled.12",
-        xltm: "application/vnd.ms-excel.template.macroEnabled.12",
-        xlam: "application/vnd.ms-excel.addin.macroEnabled.12",
-        xlsb: "application/vnd.ms-excel.sheet.binary.macroEnabled.12",
-        xslt: "application/xslt+xml",
-        xul: "application/vnd.mozilla.xul+xml",
-        xwd: "image/x-xwindowdump",
-        xyz: "chemical/x-xyz",
-        zip: "application/zip"
-    };
-
     /**
    * Reads a File using a FileReader.
    * @param file {File} the File to read.
@@ -2790,14 +2601,13 @@
    * @param type {String} 文件的类型.
    */
     Bmob.File = function (name, data, type) {
-      var extension = /\.([^.]*)$/.exec(name);
-      if (extension == "mp4") {
-        data = data;
-      }
-      else {
-        data = data[0];
-      }
-
+        var extension = /\.([^.]*)$/.exec(name);
+        if (extension[1] == "mp4") {
+            data = data;
+        }
+        else {
+            data = data[0];
+        }
         this._name = name;
         // this._name = encodeBase64(utf16to8(name));
         var currentUser = Bmob.User.current();
@@ -2811,8 +2621,8 @@
             extension = extension[1].toLowerCase();
         }
 
-        console.log(extension);
-        var guessedType = type || mimeTypes[extension] || "text/plain";
+        // var guessedType = type || mimeTypes[extension] || "text/plain";
+        var guessedType = type || "text/plain";
         this._guessedType = guessedType;
 
         if (typeof (File) !== "undefined" && data instanceof File) {
@@ -3376,11 +3186,11 @@
                 var self = this;
                 Bmob._objectEach(serverData,
                     function (value, key) {
-                      if (value.__type != "Object") {
-                        self._serverData[key] = Bmob._decode(key, value);
-                      } else {
-                        self._serverData[key] = value;
-                      }
+                      if (value==null || value.__type != "Object") {
+                            self._serverData[key] = Bmob._decode(key, value);
+                        } else {
+                            self._serverData[key] = value;
+                        }
                     });
 
                 // Refresh the attributes.
@@ -4192,22 +4002,22 @@
         });
 
         var data = _.map(objects,
-          function (object) {
-            var json = object._getSaveJSON();
-            var method = "POST";
+            function (object) {
+                // var json = object._getSaveJSON();
+                var method = "POST";
 
-            var path = "/1/classes/" + object.className;
-            if (object.id) {
-              path = path + "/" + object.id;
-              method = "DELETE";
-            }
-            object._startSave();
-            return {
-              method: method,
-              path: path,
-            };
-          })
-        var request = Bmob._request("batch", null, null, 'POST', {"requests":data});
+                var path = "/1/classes/" + object.className;
+                if (object.id) {
+                    path = path + "/" + object.id;
+                    method = "DELETE";
+                }
+                // object._startSave();
+                return {
+                    method: method,
+                    path: path,
+                };
+            })
+        var request = Bmob._request("batch", null, null, 'POST', { "requests": data });
         return request._thenRunCallbacks(options);
     };
 
@@ -5290,7 +5100,7 @@
                             model._handleSaveResult(true);
                             promise.resolve(model);
                         }
-                        );
+                    );
 
 
                     return promise._thenRunCallbacks({});
@@ -5325,6 +5135,10 @@
                 var promise = new Bmob.Promise();
                 Bmob.User.requestOpenId(code, {
                     success: function (authData) {//获取授权成功
+                        if (authData.errcode) {
+                            console.log("登陆失败,原因：", authData, '一般是Bmob后台未设置微信appId')
+                            promise.reject(authData);
+                        }
                         var platform = "weapp";
                         var user = Bmob.Object._create("_User");
                         user._linkWith(platform, authData).then(function (resp) {
@@ -5342,6 +5156,71 @@
                 );
                 return promise._thenRunCallbacks({});
 
+            },
+            getUserInfo: function (userInfo) {
+                // var userInfo = result.userInfo
+                console.log(userInfo)
+                var nickName = userInfo.nickName
+                var avatarUrl = userInfo.avatarUrl
+                var u = Bmob.Object.extend('_User')
+                var query = new Bmob.Query(u)
+
+                var currentUser = Bmob.User.current()
+                if (!currentUser) {
+                    console.log('未获取到用户信息')
+                }
+                var openid = wx.getStorageSync('openid')
+                console.log(openid)
+                query.get(currentUser.id, {
+                    success: function (result) {
+                        result.set('nickName', nickName)
+                        result.set('userPic', avatarUrl)
+                        result.set('openid', openid)
+                        result.save().then((res) => {
+                            // var currentUser = Bmob.User.current()
+                            currentUser.set('nickName', nickName)
+                            currentUser.set('userPic', avatarUrl)
+                            Bmob.User._saveCurrentUser(currentUser)
+                        })
+                    }
+                })
+
+            },
+            auth: function () {
+                var that = this;
+                var promise = new Bmob.Promise();
+                wx.checkSession({
+                    success: function () {
+                        var openid =wx.getStorageSync('openid')
+                        if (!openid) {
+                            console.log('本地openid为空')
+                            promise.reject('登陆错误，请在Bmob后台填写小程序AppSecret。');
+                            return false;
+                        }
+                        var user =Bmob.User.current();
+                        promise.resolve(user);
+                    },
+                    fail: function () {
+                        wx.login({
+                            success: function (res) {
+
+                                that.loginWithWeapp(res.code).then(
+                                    function (user) {
+
+                                        var openid = user.get('authData').weapp.openid
+                                        wx.setStorageSync('openid', openid)
+                                        //保存用户其他信息到用户表
+                                        promise.resolve(user);
+                                    },
+                                    function (err) {
+                                        promise.reject(err);
+                                    }
+                                )
+                            }
+                        })
+                    }
+                })
+                return promise._thenRunCallbacks({});
             },
 
 
@@ -5880,6 +5759,29 @@
         var query = new Bmob.Query(className);
         query._orQuery(queries);
         return query;
+    };
+
+    Bmob.Query.Bql = function (bql, pvalues, options) {
+        var params = { bql: bql };
+        if (_.isArray(pvalues)) {
+            params.pvalues = pvalues;
+        } else {
+            options = pvalues;
+        }
+
+        var request = Bmob._request('cloudQuery', null, null, 'GET', params, options);
+        return request.then(function (response) {
+            //query to process results.
+            var query = new Bmob.Query('bmob');
+            var results = _.map(response.results, function (json) {
+                var obj = query._newObject(response);
+                if (obj._finishFetch) {
+                    obj._finishFetch(query._processResult(json), true);
+                }
+                return obj;
+            });
+            return results;
+        });
     };
 
     Bmob.Query._extend = Bmob._extend;
@@ -6930,7 +6832,7 @@
 */
     Bmob.generateCode = Bmob.generateCode || {};
     Bmob.generateCode = function (data, options) {
-      var request = Bmob._request("wechatApp/qr/generatecode", null, null, 'POST', Bmob._encode(data, null, true));
+        var request = Bmob._request("wechatApp/qr/generatecode", null, null, 'POST', Bmob._encode(data, null, true));
         return request.then(function (resp) {
             return Bmob._decode(null, resp);
         })._thenRunCallbacks(options);
@@ -6939,16 +6841,31 @@
 
 
     /**
+* @namespace 退款函数
+*/
+    Bmob.refund = Bmob.refund || {};
+    Bmob.refund = function (data, options) {
+        var request = Bmob._request("pay/refund", null, null, 'POST', Bmob._encode(data, null, true));
+        return request.then(function (resp) {
+            return Bmob._decode(null, resp);
+        })._thenRunCallbacks(options);
+
+    }
+
+
+
+
+    /**
     * @namespace 发送模板消息
     */
     Bmob.sendMessage = Bmob.sendMessage || {};
     Bmob.sendMessage = function (data, options) {
 
-      var request = Bmob._request("wechatApp/SendWeAppMessage", null, null, 'POST', Bmob._encode(data, null, true));
+        var request = Bmob._request("wechatApp/SendWeAppMessage", null, null, 'POST', Bmob._encode(data, null, true));
 
-      return request.then(function (resp) {
-        return Bmob._decode(null, resp);
-      })._thenRunCallbacks(options);
+        return request.then(function (resp) {
+            return Bmob._decode(null, resp);
+        })._thenRunCallbacks(options);
 
     }
 
@@ -6958,11 +6875,11 @@
     Bmob.sendMasterMessage = Bmob.sendMasterMessage || {};
     Bmob.sendMasterMessage = function (data, options) {
 
-      var request = Bmob._request("wechatApp/notifyMsg", null, null, 'POST', Bmob._encode(data, null, true));
+        var request = Bmob._request("wechatApp/notifyMsg", null, null, 'POST', Bmob._encode(data, null, true));
 
-      return request.then(function (resp) {
-        return Bmob._decode(null, resp);
-      })._thenRunCallbacks(options);
+        return request.then(function (resp) {
+            return Bmob._decode(null, resp);
+        })._thenRunCallbacks(options);
 
     }
 
